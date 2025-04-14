cee8e21548be322b0adeb255254a95b49e26ccf2
ShivanHussain <shivanhussain6@gmail.com>
2025-04-14 11:19:37 +0530

    Update dbconn.js
    change the database

--- Backend/database/dbconn.js
+++ Backend/database/dbconn.js
@@ -4,7 +4,7 @@ const URL1 = process.env.MONGO_URL;
 const URL2 = process.env.ATLAS_URL;
 
 const dbconn = async () => {
-    mongoose.connect(process.env.MONGO_URL,{
+    mongoose.connect(process.env.ATLAS_URL,{
         dbName: "Spandan"
     }).then(()=>{
         console.log(`Connected to MongoDB`);
